45666fe2371f595d2ed6a235fa358d540963af02
Shivam Tiwari <114647321+MR-Shivam-Tiwari@users.noreply.github.com>
2024-07-19 17:43:01 +0530

    push

--- models/participant.js
+++ models/participant.js
@@ -1,12 +1,21 @@
 const mongoose = require('mongoose');
 
-const eventSchema = new mongoose.Schema({
+const participantSchema = new mongoose.Schema({
+    participantId: { type: String, required: true, unique: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    designation: { type: String, required: true },
+    institute: { type: String, required: true },
+    idCardType: { type: String },
+    backgroundImage: { type: String, required: true },
+    profilePicture: { type: String, required: true },
+    eventId: { type: String, required: true },
     eventName: { type: String, required: true },
-    address: { type: String, required: true },
-    date: { type: Date, required: true },
-    photoUrl: { type: String },
-    categories: [{ type: String }],
-    archive: { type: Boolean, default: false } // New field for archiving event
+    archive: { type: Boolean, default: false } 
+}, {
+    timestamps: true
 });
 
-module.exports = mongoose.model('Event', eventSchema);
+const Participant = mongoose.model('Participant', participantSchema);
+
+module.exports = Participant;
